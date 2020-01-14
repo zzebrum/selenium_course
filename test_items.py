c3ec015ccb9df95341679a3e69df40527c1f7765
zzebrum <44832195+zzebrum@users.noreply.github.com>
2020-01-14 23:41:05 +0300

    Update test_items.py
    minor fixes

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ import time
 def test_add_button_availability_in_different_languages(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = len(browser.find_elements_by_css_selector("#add_to_basket_form>.btn"))
-    assert button > 0, "no button"
+    button = browser.find_elements_by_css_selector("#add_to_basket_form>.btn")
+    assert len(button) > 0, "no button"
