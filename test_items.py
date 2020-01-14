3ac16e53f0b513fe38b137f25b0b4c392f7f5881
zzebrum <44832195+zzebrum@users.noreply.github.com>
2020-01-14 17:56:46 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import pytest
 import time
 
-def test_lang(browser):
+def test_add_button_availability_in_different_languages(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = len(browser.find_elements_by_css_selector("#add_to_basket_form>.btn"))
