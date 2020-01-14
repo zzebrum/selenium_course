99c3a28afe489f8d91a5196b6a9bfc368739525b
zzebrum <44832195+zzebrum@users.noreply.github.com>
2020-01-14 19:12:56 +0300

    Update conftest.py
    added option for chrome/firefox

--- conftest.py
+++ conftest.py
@@ -1,21 +1,36 @@
-
-from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--language', \
-                    action = 'store', \
-                    default = None, \
-                    help = "Choose language: ru, en, ...")
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language',
+                     action='store',
+                     default=None,
+                     help="Chose language:ru, en, fr, es")
 
 
-@pytest.fixture(scope = "function")
+@pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', \
-                                    {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    # browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test, language =", user_language)
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        print("\nstart firefox browser for test, language =", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("chose language and browser")
     yield browser
+    print("\nquit browser..")
     browser.quit()
